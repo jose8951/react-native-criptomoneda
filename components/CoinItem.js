290860d60900d5274396d2e5626dc0decb3d1157
jose8951 <josves222@gmail.com>
2021-07-20 12:12:52 +0200

    version 2

--- components/CoinItem.js
+++ components/CoinItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
-
+//moneda 
 const CoinItem = ({ coin }) => {
   return (
     <View style={styles.container}>
